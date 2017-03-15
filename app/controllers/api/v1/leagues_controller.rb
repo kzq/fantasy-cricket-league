f062ec71cd1e08a7e60969b446e3b496f5e735a6
Kamran Zafar <kami_ravian@yahoo.com>
2017-03-15 20:58:11 +0000

    Make leagues list public

--- app/controllers/api/v1/leagues_controller.rb
+++ app/controllers/api/v1/leagues_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::LeaguesController < ApplicationController
+  skip_before_action :authenticate_request, only: [:index]
+  
   def index
     leagues = League.all
     render json: leagues, status: 200     
