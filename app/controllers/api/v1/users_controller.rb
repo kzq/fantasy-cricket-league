081fb7cb1aa9477b8b4290022bfd0d1e2c558319
Kamran Zafar <kami_ravian@yahoo.com>
2017-03-15 23:18:12 +0000

    wip

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,19 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authenticate_request, only: [:create]
   
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created  
+    else  
+      render_error(user, :unprocessable_entity) 
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :firstname, :surname)
+  end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, constraints: { subdomian: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :leagues, only: [:index]
-      resources :users, only: [:index]
+      resources :users, only: [:create]
       post 'login' => 'authentication#authenticate_user'   
     end
   end
