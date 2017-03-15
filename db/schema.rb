e3623f156b7865ee6189aa98e8b61e1b681e2932
Kamran Zafar <kami_ravian@yahoo.com>
2017-03-15 22:03:49 +0000

    Remove token field from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313232946) do
+ActiveRecord::Schema.define(version: 20170315220124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,12 +29,10 @@ ActiveRecord::Schema.define(version: 20170313232946) do
     t.string   "email"
     t.string   "firstname"
     t.string   "surname"
-    t.string   "token"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["token"], name: "index_users_on_token", unique: true, using: :btree
   end
 
 end
