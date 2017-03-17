c1c7b7f7046546f9d25f3f4bba97f36ddbc794bb
Kamran Zafar <kami_ravian@yahoo.com>
2017-03-17 15:26:31 +0000

    Create new leagues

--- app/controllers/api/v1/leagues_controller.rb
+++ app/controllers/api/v1/leagues_controller.rb
@@ -1,8 +1,41 @@
 class Api::V1::LeaguesController < ApplicationController
   skip_before_action :authenticate_request, only: [:index]
   
+  # GET /leagues 
   def index
     leagues = League.all
     render json: leagues, status: 200     
   end
+  
+  #POSt /leagues
+  def create
+    league = League.new(league_params)
+    if league.save
+      render json: league, status: :created
+    else
+      render json: league.errors, status: :unprocessable_entity
+    end      
+  end
+  
+  # GET /leagues/:id
+  def show
+    
+  end  
+  
+  # PUT /leagues/:id
+  def update
+    
+  end
+  
+  # DELETE /leagues/:id
+  def destroy
+    
+  end
+  
+  private
+  
+  def league_params
+    params.require(:league).permit(:name, :country, :description)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'api_constraints'
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, constraints: { subdomian: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :leagues, only: [:index]
+      resources :leagues, only: [:index, :create]
       resources :users, only: [:create]
       post 'login' => 'authentication#authenticate_user'   
     end
